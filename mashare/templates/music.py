f5806f28f3b056ad6e16ef0985d10d16312c5952
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-16 05:35:09 +0330

    add music player

---
+++ mashare/templates/music.py
@@ -0,0 +1,20 @@
+from inui.elements import *
+
+from .base import base
+from .utils import link_maker, process_path
+
+
+def music(path: str):
+    return str(
+        base(
+            """<div class="music-player">
+        <audio id="audio" src="%s" controls></audio>
+        <div class="controls">
+            <button id="play-pause">Play</button>
+            <button id="stop">Stop</button>
+        </div>
+    </div>
+    <script src="/assets/js/music.js"></script>"""%link_maker(path=path,format="dl"),
+            path=path,
+        ),
+    )
