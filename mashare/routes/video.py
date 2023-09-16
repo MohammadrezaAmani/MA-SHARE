9d530a5b7fb90e81c30c153d0392f039f9bd4788
MohammadrezaAmani <more.amani@yahoo.com>
2023-09-16 05:35:32 +0330

    add video player

---
+++ mashare/routes/video.py
@@ -0,0 +1,22 @@
+"main router"
+
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+# from mashare.templates.templates import template
+from mashare.templates.video import video_player
+from mashare import BASE_PATH
+
+router = APIRouter(
+    prefix="/video",
+    tags=["video"],
+    default_response_class={404: {"message": "not found"}},
+)
+router.mount(BASE_PATH + "/assets", StaticFiles(directory="assets"), name="assets")
+
+
+@router.get("/{file_path:path}")
+async def video_play(file_path: str):
+    "main router"
+    return HTMLResponse(video_player("/" + file_path))
---
+++ mashare/templates/video.py
@@ -0,0 +1,85 @@
+from inui.elements import Div, Video, Button, I, Script
+from .base import base
+
+
+def video_player(path):
+    return base(
+        str(
+            Div(
+                classs="""container""",
+                data=(
+                    Video(
+                        onclick="""play(event)""",
+                        src="/dl" + path,
+                        id="""video""",
+                    ),
+                    Div(
+                        classs="""controls""",
+                        data=(
+                            Button(
+                                onclick="""play(event)""",
+                                data=(
+                                    I(
+                                        classs="""fa fa-play""",
+                                    ),
+                                    I(
+                                        classs="""fa fa-pause""",
+                                    ),
+                                ),
+                            ),
+                            Button(
+                                onclick="""rewind(event)""",
+                                data=(
+                                    I(
+                                        classs="""fa fa-fast-backward""",
+                                    ),
+                                ),
+                            ),
+                            Div(
+                                classs="""timeline""",
+                                data=(
+                                    Div(
+                                        classs="""bar""",
+                                        data=(
+                                            Div(
+                                                classs="""inner""",
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            Button(
+                                onclick="""forward(event)""",
+                                data=(
+                                    I(
+                                        classs="""fa fa-fast-forward""",
+                                    ),
+                                ),
+                            ),
+                            Button(
+                                onclick="""fullScreen(event)""",
+                                data=(
+                                    I(
+                                        classs="""fa fa-expand""",
+                                    ),
+                                ),
+                            ),
+                            Button(
+                                onclick="""download(event)""",
+                                data=(
+                                    I(
+                                        classs="""fa fa-cloud-download""",
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            Script(
+                src="""/assets/js/video.min.js""",
+            ),
+        ),
+        style="""/assets/css/video.min.css""",
+        path=path,
+    )
